c944c6c07880cfdc9b1fe799874f5a4adfe0107e
Hasham <hasham.hasham1@gmail.com>
2020-04-18 13:41:54 +0500

    BurgerBuilder component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./container/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
-          <h1>Hashaam</h1>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
---
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Auxiliary";
+class BugerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div> Burger </div>
+        <div>Burger Tools</div>
+      </Aux>
+    );
+  }
+}
+
+export default BugerBuilder;
