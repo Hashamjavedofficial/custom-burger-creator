0eaf93da3da50066faf2661ff3e9fb8cd6430514
Hasham <hasham.hasham1@gmail.com>
2020-05-18 23:10:31 +0500

    beforeEach() , setProps({})

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -5,10 +5,17 @@ import NavigationItems from "./NavigationItems";
 import NavigationItem from "./NavigationItem/NavigationItem";
 
 configure({ adapter: new Adapter() });
+let wrapper = null;
+beforeEach(() => {
+  wrapper = shallow(<NavigationItems />);
+});
 
 describe("NavigationItems", () => {
   it("Single test for navigation is not auth", () => {
-    const wrapper = shallow(<NavigationItems />);
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+  it("Single test for navigation is not auth", () => {
+    wrapper.setProps({ isAuth: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 });
